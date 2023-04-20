d755b42a440c501297981b7511b65c8cb57e4455
The-Coder-Kishor <58686913+The-Coder-Kishor@users.noreply.github.com>
2023-04-20 12:29:54 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,17 +3,13 @@ import './app.css'
 import axios from 'axios'
 import { Button, Container, TextField, Typography } from '@mui/material'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { First_Time_File } from './first_time_file'
 import { Home } from './home'
-import { Recur } from './recur'
 
 export function App() {
   return (
     <Router>
       <Routes>
         <Route exact path='/' element = {<Home />} />
-        <Route path='/first_time_file' element = {<First_Time_File />} />
-        <Route path='/recur' element = {<Recur />} />
       </Routes>
     </Router>
   )
